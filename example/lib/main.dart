af7e92395d692a1f50c2d10fdc15965fbbb85e56
Bruno Tavares <connect+github@bltavares.com>
2020-04-03 23:31:22 -0300

    Initial work done. Can open DB and start logs. Crashes on db.set; Requires custom sled-native build

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,45 +1,14 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:path_provider/path_provider.dart';
 
-import 'package:flutter/services.dart';
-import 'package:treno/treno.dart';
+import 'package:treno/treno.dart' as treno;
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() {
+  treno.startLogger();
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await Treno.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -47,8 +16,24 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Builder(
+          builder: (BuildContext context) {
+            return Center(
+              child: MaterialButton(
+                child: Text("Try me!"),
+                onPressed: () async {
+                  final directory = await getApplicationDocumentsDirectory();
+                  var config = treno.Config("${directory.path}/asdf.db");
+                  var db = treno.Db(config);
+                  db.setKey("banana", "banana");
+                  var response = db.getKey("banana");
+                  // Scaffold.of(context).showSnackBar(
+                  //     SnackBar(content: Text("Response $response")));
+                  db.dispose();
+                },
+              ),
+            );
+          },
         ),
       ),
     );
--- lib/treno.dart
+++ lib/treno.dart
@@ -3,62 +3,58 @@ import 'dart:io';
 import 'package:ffi/ffi.dart';
 
 final ffi.DynamicLibrary _sledNative = Platform.isAndroid
-    ? ffi.DynamicLibrary.open("libsled_native.so")
+    ? ffi.DynamicLibrary.open("libsled.so")
     : ffi.DynamicLibrary.process();
 
-final ffi.Pointer Function() _createConfig = _sledNative
-    .lookup<ffi.NativeFunction<ffi.Pointer Function()>>("sled_create_config")
-    .asFunction();
+final _createConfig =
+    _sledNative.lookupFunction<ffi.Pointer Function(), ffi.Pointer Function()>(
+        "sled_create_config");
 
-final void Function(ffi.Pointer) _freeConfig = _sledNative
-    .lookup<ffi.NativeFunction<ffi.Void Function(ffi.Pointer)>>(
-        "sled_free_config")
-    .asFunction();
+final _freeConfig = _sledNative.lookupFunction<ffi.Void Function(ffi.Pointer),
+    void Function(ffi.Pointer)>("sled_free_config");
 
-final void Function(ffi.Pointer, ffi.Pointer<Utf8>) _configSetPath = _sledNative
-    .lookup<
-        ffi.NativeFunction<
-            ffi.Void Function(
-                ffi.Pointer, ffi.Pointer<Utf8>)>>("sled_config_set_path")
-    .asFunction();
+final _configSetPath = _sledNative.lookupFunction<
+    ffi.Pointer Function(ffi.Pointer, ffi.Pointer<Utf8>),
+    ffi.Pointer Function(
+        ffi.Pointer, ffi.Pointer<Utf8>)>("sled_config_set_path");
 
-final void Function(ffi.Pointer, int) _configSetReadOnly = _sledNative
-    .lookup<ffi.NativeFunction<ffi.Void Function(ffi.Pointer, ffi.Uint8)>>(
-        "sled_config_read_only")
-    .asFunction();
+final _configSetReadOnly = _sledNative.lookupFunction<
+    ffi.Void Function(ffi.Pointer, ffi.Uint8),
+    void Function(ffi.Pointer, int)>("sled_config_read_only");
 
-final void Function(ffi.Pointer, int) _configSetCacheCapacity = _sledNative
-    .lookup<ffi.NativeFunction<ffi.Void Function(ffi.Pointer, ffi.Uint32)>>(
-        "sled_config_set_cache_capacity")
-    .asFunction();
+final _configSetCacheCapacity = _sledNative.lookupFunction<
+    ffi.Void Function(ffi.Pointer, ffi.Uint32),
+    void Function(ffi.Pointer, int)>("sled_config_set_cache_capacity");
 
-final void Function(ffi.Pointer, int) _configUseCompression = _sledNative
-    .lookup<ffi.NativeFunction<ffi.Void Function(ffi.Pointer, ffi.Uint8)>>(
-        "sled_config_use_compression")
-    .asFunction();
+final _configUseCompression = _sledNative.lookupFunction<
+    ffi.Void Function(ffi.Pointer, ffi.Uint8),
+    void Function(ffi.Pointer, int)>("sled_config_use_compression");
 
-final void Function(ffi.Pointer, int) _configFlushEveryMs = _sledNative
-    .lookup<ffi.NativeFunction<ffi.Void Function(ffi.Pointer, ffi.Uint64)>>(
-        "sled_config_flush_every_ms")
-    .asFunction();
+final _configFlushEveryMs = _sledNative.lookupFunction<
+    ffi.Void Function(ffi.Pointer, ffi.Uint64),
+    void Function(ffi.Pointer, int)>("sled_config_flush_every_ms");
 
 class Config {
   ffi.Pointer _configPointer = _createConfig();
 
   Config(
     String path, {
-    bool readOnly = false,
-    int cacheCapacity = 200,
-    bool useCompression = true,
-    int flushEveryMs = 1000,
+    // TODO: Figure out the defaults
+    // bool readOnly = false,
+    // int cacheCapacity = 200,
+    // int flushEveryMs = 1000,
+
+    /// The compression feature must be enabled on the lib
+    bool useCompression = false,
   }) {
-    _configSetPath(this._configPointer, Utf8.toUtf8(path));
-    _configSetReadOnly(this._configPointer, readOnly ? 1 : 0);
-    if (cacheCapacity != 0) {
-      _configSetCacheCapacity(this._configPointer, cacheCapacity);
-    }
-    _configUseCompression(this._configPointer, useCompression ? 1 : 0);
-    _configFlushEveryMs(this._configPointer, flushEveryMs);
+    this._configPointer =
+        _configSetPath(this._configPointer, Utf8.toUtf8(path));
+    // _configSetReadOnly(this._configPointer, readOnly ? 1 : 0);
+    // if (cacheCapacity != 0) {
+    //   _configSetCacheCapacity(this._configPointer, cacheCapacity);
+    // }
+    // _configUseCompression(this._configPointer, useCompression ? 1 : 0);
+    // _configFlushEveryMs(this._configPointer, flushEveryMs);
   }
 
   ffi.Pointer _consume() {
@@ -74,53 +70,49 @@ class Config {
   }
 }
 
-final ffi.Pointer Function(ffi.Pointer) _openDb = _sledNative
-    .lookup<ffi.NativeFunction<ffi.Pointer Function(ffi.Pointer)>>(
-        "sled_open_db")
-    .asFunction();
+final _openDb = _sledNative.lookupFunction<ffi.Pointer Function(ffi.Pointer),
+    ffi.Pointer Function(ffi.Pointer)>("sled_open_db");
 
-final void Function(ffi.Pointer) _closeDb = _sledNative
-    .lookup<ffi.NativeFunction<ffi.Void Function(ffi.Pointer)>>("sled_close")
-    .asFunction();
+final _closeDb = _sledNative.lookupFunction<ffi.Void Function(ffi.Pointer),
+    void Function(ffi.Pointer)>("sled_close");
 
-final void Function(
+final _set = _sledNative.lookupFunction<
+    ffi.Void Function(
+  ffi.Pointer, // Db
+  ffi.Pointer<Utf8>, // Key value
+  ffi.Uint64, // Key len
+  ffi.Pointer<Utf8>, // Value value
+  ffi.Uint64, // Value len
+),
+    void Function(
   ffi.Pointer, // Db
   ffi.Pointer<Utf8>, // Key value
   int, // Key len
   ffi.Pointer<Utf8>, // Value value
   int, // Value len
-) _set = _sledNative
-    .lookup<
-        ffi.NativeFunction<
-            ffi.Void Function(
-      ffi.Pointer, // Db
-      ffi.Pointer<Utf8>, // Key value
-      ffi.Uint64, // Key len
-      ffi.Pointer<Utf8>, // Value value
-      ffi.Uint64, // Value len
-    )>>("sled_set")
-    .asFunction();
+)>("sled_set");
 
-final ffi.Pointer<Utf8> Function(
+final _get = _sledNative.lookupFunction<
+    ffi.Pointer<Utf8> Function(
+  ffi.Pointer,
+  ffi.Pointer<Utf8>,
+  ffi.Uint64,
+  ffi.Pointer<ffi.Uint64>,
+),
+    ffi.Pointer<Utf8> Function(
   ffi.Pointer,
   ffi.Pointer<Utf8>,
   int,
   ffi.Pointer<ffi.Uint64>,
-) _get = _sledNative
-    .lookup<
-        ffi.NativeFunction<
-            ffi.Pointer<Utf8> Function(
-      ffi.Pointer,
-      ffi.Pointer<Utf8>,
-      ffi.Uint64,
-      ffi.Pointer<ffi.Uint64>,
-    )>>("sled_get")
-    .asFunction();
+)>("sled_get");
 
-final void Function(ffi.Pointer<Utf8>) _freeBuffer = _sledNative
-    .lookup<ffi.NativeFunction<ffi.Void Function(ffi.Pointer<Utf8>)>>(
-        "sled_free_buf")
-    .asFunction();
+final _freeBuffer = _sledNative.lookupFunction<
+    ffi.Void Function(ffi.Pointer<Utf8>),
+    void Function(ffi.Pointer<Utf8>)>("sled_free_buf");
+
+final startLogger =
+    _sledNative.lookupFunction<ffi.Void Function(), void Function()>(
+        "camarim_setup_logger");
 
 class Db {
   ffi.Pointer _dbPointer;
